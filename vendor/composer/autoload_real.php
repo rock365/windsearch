f3c4a3ec0c237f8ccd5f99c217c2e20d01b2a6c0
rock365 <1593250826@qq.com>
2025-02-21 14:47:10 +0800

    优化

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitaa7bc86073386447435278c4e4e3ebd5
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitaa7bc86073386447435278c4e4e3ebd5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitaa7bc86073386447435278c4e4e3ebd5', 'loadClassLoader'));
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rock365/windsearch',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd27a81e7dbb3a0719794396cbf62fd953f0e7c82',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rock365/windsearch' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd27a81e7dbb3a0719794396cbf62fd953f0e7c82',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
